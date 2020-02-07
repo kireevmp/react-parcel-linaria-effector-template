fa070774d7e3d71ef46ef206deabb0fd33c91489
Kireev Mikhail <kireevmp@ya.ru>
2020-02-07 11:05:02 +0700

    Init commit

---
+++ src/custom.d.ts
@@ -0,0 +1,19 @@
+declare module "*.ttf" {
+  const font: any;
+  export default font;
+}
+
+declare module "*.png" {
+  const image: any;
+  export default image;
+}
+
+declare module "*.jpg" {
+  const image: any;
+  export default image;
+}
+
+declare module "*.svg" {
+  const svg: any;
+  export default svg;
+}
